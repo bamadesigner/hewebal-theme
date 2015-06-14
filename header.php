3bdd695b385fe3fae5fd9f83fd2630581bc44fcd
Rachel Carden <bamadesigner@gmail.com>
2015-06-13 22:06:05 -0500

    Fixed a wp_title() issue

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title><?php wp_title( '-', true, left ); ?></title>
+    <title><?php wp_title( '-', true, 'left' ); ?></title>
 
     <?php wp_head(); ?>
 
