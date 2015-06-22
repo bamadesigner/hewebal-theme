f6e9163bd307abe716495a72af26d6fcf0ac700c
Rachel Carden <bamadesigner@gmail.com>
2015-06-22 13:30:03 -0500

    Adding the mStoner sponsor ad

--- sponsors.php
+++ sponsors.php
@@ -2,6 +2,8 @@
 
 // Template Name: HEWEBAL Sponsors
 
+$sponsor_images = get_stylesheet_directory_uri() . '/images/sponsors/';
+
 get_header();
 
 if ( have_posts() ) :
@@ -25,14 +27,14 @@ if ( have_posts() ) :
             </div>
         </div>
 
-        <h2>Graduate Sponsors</h2>
-        <div class="row">
-            <div class="col-md-4">
-                <img src="http://placehold.it/540x400">
-            </div>
-            <div class="col-md-4"></div>
-            <div class="col-md-4"></div>
-        </div><?php
+	    <div class="graduate-sponsors">
+	        <h2>Graduate Sponsors</h2>
+	        <div class="row">
+	            <div class="col-md-12">
+	                <img src="<?php echo $sponsor_images; ?>mStoner_banner_540x400_colorOption2.gif" />
+	            </div>
+	        </div>
+	    </div><?php
 
     endwhile;
 endif;
