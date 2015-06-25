2ee5bf66bd1c76dc29bc6db440f83c1d77a26799
Rachel Carden <bamadesigner@gmail.com>
2015-06-25 01:06:04 -0500

    Setting up the home page and adding sponsors to the footer

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php
+
+get_header();
+
+?><h1 class="welcome">Welcome to the 2015 HighEdWeb Alabama Regional Conference!</h1>
+
+<div class="row">
+    <div class="col-md-8">
+
+        <h2>About the Conference</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus urna, ullamcorper in ullamcorper in, dapibus vel leo. Nam diam odio, aliquam quis accumsan a, viverra non sem. Pellentesque non fringilla sapien.</p>
+
+        <h2>About the Conference</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus urna, ullamcorper in ullamcorper in, dapibus vel leo. Nam diam odio, aliquam quis accumsan a, viverra non sem. Pellentesque non fringilla sapien.</p>
+
+        <h2>About the Conference</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus urna, ullamcorper in ullamcorper in, dapibus vel leo. Nam diam odio, aliquam quis accumsan a, viverra non sem. Pellentesque non fringilla sapien.</p>
+
+    </div>
+    <div class="col-md-4">
+        <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/hashtag/hewebAL" data-widget-id="613945196527677440">#hewebAL Tweets</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    </div>
+</div><?php
+
+get_footer();
