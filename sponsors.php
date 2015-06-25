b6c0403c5981c4667a9363f36354a29f036dc62c
Rachel Carden <bamadesigner@gmail.com>
2015-06-24 23:38:54 -0500

    Finishing the sponsors page

--- sponsors.php
+++ sponsors.php
@@ -10,28 +10,18 @@ if ( have_posts() ) :
     while ( have_posts() ) :
         the_post();
 
-        ?><h2>Bronze Sponsors</h2>
-        <div class="row">
-            <div class="col-sm-6 col-md-6">
-                <img src="http://placehold.it/540x400" />
-            </div>
-            <div class="col-sm-6 col-md-6">
-                <img src="http://placehold.it/540x400" />
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6 col-md-6">
-                <img src="http://placehold.it/540x400" />
-            </div>
+        ?><p class="sponsor-thank-you">A <span class="huge">HUGE</span> thank you to all of our sponsors that help support the advancement of higher education web professionals!</p>
+
+        <div class="sponsors bronze">
+            <h2 class="sponsor-header">Bronze Sponsors</h2>
+            <a href="http://www.hannonhill.com/"><img src="<?php echo $sponsor_images; ?>Hannon-Hill-ad.jpg" /></a>
+            <a href="http://www.omniupdate.com/"><img src="<?php echo $sponsor_images; ?>OmniUpdate-Program-Ad.png" /></a>
+            <a href="http://ua.edu"><img class="ua-sponsor-logo" src="<?php echo $sponsor_images; ?>univ-of-alabama.png" /></a>
         </div>
 
-	    <div class="graduate-sponsors">
-	        <h2>Graduate Sponsors</h2>
-	        <div class="row">
-	            <div class="col-md-12">
-	                <img src="<?php echo $sponsor_images; ?>mStoner_banner_540x400_colorOption2.gif" />
-	            </div>
-	        </div>
+	    <div class="sponsors graduate">
+	        <h2 class="sponsor-header">Graduate Sponsors</h2>
+            <a href="http://mstnr.me/1QBWNDG"><img src="<?php echo $sponsor_images; ?>mStoner_banner_540x400_colorOption2.gif" /></a>
 	    </div><?php
 
     endwhile;
