3adaa931e99aee4cd0f65df6d10257860206388c
Rachel Carden <bamadesigner@gmail.com>
2015-06-25 09:00:59 -0500

    Adding some more text to the home page

--- front-page.php
+++ front-page.php
@@ -9,16 +9,15 @@ get_header();
 
         <h2>About the Conference</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus urna, ullamcorper in ullamcorper in, dapibus vel leo. Nam diam odio, aliquam quis accumsan a, viverra non sem. Pellentesque non fringilla sapien.</p>
-
-	    <a class="btn btn-primary btn-block" href="https://hewebal.com/about/">Learn more about #hewebAL</a>
+		<a class="btn btn-default btn-block" href="https://hewebal.com/about/">Learn more about HighEdWeb Alabama</a>
 
         <h2>Become A HighEdWeb Member</h2>
         <p>Members are the strength of our organization and, as part of membership in the <a href="http://highedweb.org/">HighEdWeb Association</a>, members have access to exclusive content and a growing community. Members receive access to exclusive content within the members-only website, including keynote videos from <a href="http://2015.highedweb.org/">the Annual Conference</a>, discounted registration for events such as the HighEdWeb Annual Conference, and advance notifications about special events, conferences, development opportunities and more.</p>
+		<a class="btn btn-default btn-block" href="https://membership.highedweb.org/">Become a HighEdWeb Member</a>
 
-	    <a class="btn btn-primary btn-block" href="https://membership.highedweb.org/">Become a HighEdWeb Member</a>
-
-        <h2>About the Conference</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus urna, ullamcorper in ullamcorper in, dapibus vel leo. Nam diam odio, aliquam quis accumsan a, viverra non sem. Pellentesque non fringilla sapien.</p>
+        <h2>Join Us At HighEdWeb 2015 Annual Conference</h2>
+	    <p>HighEdWeb Alabama is a regional conference, which allows us to meet and learn from local professionals in a smaller group, but we also hope you'll join us at the annual HighEdWeb Conference, which will take place October 4-7, 2015 in the beautiful city of Milwaukee. We anticipate a conference attendance of about 750 Web professionals from private and public colleges and universities across the U.S. and beyond.</p>
+		<a class="btn btn-default btn-block" href="http://2015.highedweb.org/">Learn more about the 2015 Annual Conference</a>
 
     </div>
     <div class="col-md-4">
