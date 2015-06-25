6af1d944c04b0ae1dcae3c954033e665003ff3c0
Rachel Carden <bamadesigner@gmail.com>
2015-06-24 20:06:30 -0500

    Setting up the footer

--- footer.php
+++ footer.php
@@ -5,7 +5,19 @@
         </div> <!-- #hewebal-content -->
     </div> <!-- #hewebal-main -->
 
-    <div id="hewebal-footer"></div> <!-- #hewebal-footer -->
+    <div id="hewebal-footer">
+        <div class="container-fluid">
+
+            <a href="http://www.highedweb.org/"><img class="heweb-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/heweb-logo-white.png" /></a>
+            <p>HighEdWeb is an organization of Web professionals working at institutions of higher education. We design, develop, manage and map the futures of higher education websites.</p>
+            <ul class="footer-list">
+                <li>&copy; <?php echo date( 'Y' ); ?> <a href="http://www.highedweb.org/">HighEdWeb Professionals Association</a></li>
+                <li><a href="http://www.highedweb.org/about/anti-harassment-policy/">Code of Conduct</a></li>
+                <li><a href="https://hewebal.com/contact/">Contact Us</a></li>
+            </ul>
+
+        </div>
+    </div> <!-- #hewebal-footer -->
 
     </div> <!-- #hewebal-wrapper -->
 
