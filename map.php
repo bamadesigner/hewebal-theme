0d8abbc5528957c883943cc5d882ece7cd11ec92
Rachel Carden <bamadesigner@gmail.com>
2015-06-25 12:10:48 -0500

    Adding "View in Google" button to map

--- map.php
+++ map.php
@@ -6,7 +6,10 @@ get_header();
 
 ?><div class="hewebal-map">
     <iframe src="https://www.google.com/maps/d/embed?mid=zexjgVPyBHAI.kJVG1uo3hCww"></iframe>
-    <span class="view-fullscreen btn btn-primary">View Fullscreen</span>
+	<div class="hewebal-map-buttons">
+	    <span class="view-fullscreen btn btn-primary">View Fullscreen</span>
+		<a class="view-goggle btn btn-primary" href="http://bit.ly/hewebal-map">View in Google</a>
+	</div>
 </div><?php
 
 get_footer();
