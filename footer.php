5b2e16800a72532b5f8bc665a8840c3b166582e0
Rachel Carden <bamadesigner@gmail.com>
2015-06-25 08:25:11 -0500

    Building out the home page and adding to the footer

--- footer.php
+++ footer.php
@@ -30,10 +30,11 @@
                 <img class="heweb-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/heweb-logo-white.png" alt="" />
                 <span class="screen-reader-text">The HighEdWeb Association</span>
             </a>
-            <p>HighEdWeb is an organization of Web professionals working at institutions of higher education. We design, develop, manage and map the futures of higher education websites.</p>
+            <p><span class="line">HighEdWeb is an organization of Web professionals working at institutions of higher education.</span><span class="line">We design, develop, manage and map the futures of higher education websites.</span></p>
             <ul class="footer-list">
                 <li>&copy; <?php echo date( 'Y' ); ?> <a href="http://www.highedweb.org/">HighEdWeb Professionals Association</a></li>
-                <li><a href="http://www.highedweb.org/about/anti-harassment-policy/">Code of Conduct</a></li>
+	            <li><a href="http://2015.highedweb.org/">2015 National Conference</a></li>
+	            <li><a href="http://www.highedweb.org/about/anti-harassment-policy/">Code of Conduct</a></li>
                 <li><a href="https://hewebal.com/contact/">Contact Us</a></li>
             </ul>
 
--- front-page.php
+++ front-page.php
@@ -10,8 +10,12 @@ get_header();
         <h2>About the Conference</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus urna, ullamcorper in ullamcorper in, dapibus vel leo. Nam diam odio, aliquam quis accumsan a, viverra non sem. Pellentesque non fringilla sapien.</p>
 
-        <h2>About the Conference</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus urna, ullamcorper in ullamcorper in, dapibus vel leo. Nam diam odio, aliquam quis accumsan a, viverra non sem. Pellentesque non fringilla sapien.</p>
+	    <a class="btn btn-primary btn-block" href="https://hewebal.com/about/">Learn more about #hewebAL</a>
+
+        <h2>Become A HighEdWeb Member</h2>
+        <p>Members are the strength of our organization and, as part of membership in the <a href="http://highedweb.org/">HighEdWeb Association</a>, members have access to exclusive content and a growing community. Members receive access to exclusive content within the members-only website, including keynote videos from <a href="http://2015.highedweb.org/">the Annual Conference</a>, discounted registration for events such as the HighEdWeb Annual Conference, and advance notifications about special events, conferences, development opportunities and more.</p>
+
+	    <a class="btn btn-primary btn-block" href="https://membership.highedweb.org/">Become a HighEdWeb Member</a>
 
         <h2>About the Conference</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus urna, ullamcorper in ullamcorper in, dapibus vel leo. Nam diam odio, aliquam quis accumsan a, viverra non sem. Pellentesque non fringilla sapien.</p>
