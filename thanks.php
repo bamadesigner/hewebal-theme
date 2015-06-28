0dae7ccd41ff791c989ab26cee202512c77266ae
Rachel Carden <bamadesigner@gmail.com>
2015-06-28 01:44:35 -0500

    Setup thanks page

---
+++ thanks.php
@@ -0,0 +1,70 @@
+<?php
+
+// Template Name: HEWEBAL Thanks
+
+get_header();
+
+?><p class="thanks-message">The HighEdWeb Alabama committee would like to thank the following individuals and departments for their help and support to make this event possible.</p>
+
+<div class="row">
+    <div class="col-md-5 col-md-offset-1">
+        <h2>HighEdWeb Association</h2>
+        <ul>
+            <li>Tonya Oaks Smith</li>
+            <li>Shelley Keith</li>
+            <li>Dave Olsen</li>
+            <li>Jason Woodward</li>
+            <li>Colleen Brennan-Barry</li>
+            <li>Sven Aas</li>
+            <li>Sara Clark</li>
+            <li>Marilyn Giannoccaro</li>
+        </ul>
+    </div>
+    <div class="col-md-5">
+        <h2>HighEdWeb Alabama</h2>
+        <ul>
+            <li>Rachel Carden</li>
+            <li>Rachel Thompson</li>
+            <li>John Hawkins</li>
+            <li>Brent Springer</li>
+            <li>Vincent Palmer</li>
+            <li>Lisa Sandy</li>
+            <li>Camisha Adams</li>
+            <li>Donald Malone</li>
+        </ul>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-5 col-md-offset-1">
+        <h2>Awesome Folks</h2>
+        <ul>
+            <li>Joshua Hollis</li>
+            <li>Brandon Walker</li>
+            <li>Allen Beddingfield</li>
+            <li>Rick Dowling</li>
+            <li>Amber Howard</li>
+            <li>Adam Beebe</li>
+            <li>Melanie Viering</li>
+            <li>Cole Booth</li>
+            <li>Hampton Brewer</li>
+            <li>Mo Fiorella</li>
+        </ul>
+    </div>
+    <div class="col-md-5">
+        <h2>University of Alabama</h2>
+        <ul>
+            <li>WebTide</li>
+            <li>eTech</li>
+            <li>University Relations</li>
+            <li>Student Affairs</li>
+            <li>Center for Instructional Technology</li>
+            <li>Office of Information Technology</li>
+            <li>Financial Affairs</li>
+            <li>Parking Services</li>
+        </ul>
+    </div>
+</div>
+
+<?php
+
+get_footer();
