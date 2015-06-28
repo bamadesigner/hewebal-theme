4a21a9dad513cfc5271ebdc7bde0aeb731c572be
Rachel Carden <bamadesigner@gmail.com>
2015-06-27 22:39:02 -0500

    Having "go to current" hide by default

--- js/hewebal-schedule.js
+++ js/hewebal-schedule.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($) {
 
 	// Select items that are meant to click to go to current event
-	var $go_to_current_event = jQuery( '.go-to-current-event' );
+	var $go_to_current_event = jQuery( '.go-to-current-event' ).hide();
 
-	// If no current events, then hide
-	if ( jQuery( '.schedule-row.current' ).length == 0 )
-		$go_to_current_event.hide();
+	// If current events, then show button
+	if ( jQuery( '.schedule-row.current' ).length >= 1 )
+		$go_to_current_event.show();
 
 	// If the button is clicked...
 	$go_to_current_event.on( 'click touchend', function( $event ) {
