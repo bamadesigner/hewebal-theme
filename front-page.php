cf3d6240bdb68a14058e688698ebffc904448122
Rachel Carden <bamadesigner@gmail.com>
2015-06-28 01:30:05 -0500

    Adding Flickr promo to home page

--- front-page.php
+++ front-page.php
@@ -7,9 +7,9 @@ get_header();
 <div class="row">
     <div class="col-md-8">
 
-        <?php /*<h2>About the Conference</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus urna, ullamcorper in ullamcorper in, dapibus vel leo. Nam diam odio, aliquam quis accumsan a, viverra non sem. Pellentesque non fringilla sapien.</p>
-		<a class="btn btn-default btn-block" href="https://hewebal.com/about/">Learn more about HighEdWeb Alabama</a>*/ ?>
+        <h2>About the Conference</h2>
+        <p>HighEdWeb Alabama is a regional conference of the <a href="http://www.highedweb.org/">The Higher Education Web Professionals Association</a> that is designed to bring together higher education professionals involved in all aspects of web communications - from programmers, designers, and system admins to content strategists, marketers, social media managers, and news and public relations specialists - to share knowledge, encourage best practices, and move our web communications to the next level.</p>
+		<a class="btn btn-default btn-block" href="https://hewebal.com/about/">Learn more about HighEdWeb Alabama</a>
 
         <h2>Become A HighEdWeb Member</h2>
         <p>Members are the strength of our organization and, as part of membership in the <a href="http://highedweb.org/">HighEdWeb Association</a>, members have access to exclusive content and a growing community. Members receive access to exclusive content within the members-only website, including keynote videos from <a href="http://2015.highedweb.org/">the Annual Conference</a>, discounted registration for events such as the HighEdWeb Annual Conference, and advance notifications about special events, conferences, development opportunities and more.</p>
@@ -31,12 +31,16 @@ get_header();
 		            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 				</div>
 
+                <div class="flickr-promo-wrapper">
+                    <a href="mailto:<?php echo antispambot( 'hewebalphotos@gmail.com' ); ?>"><span class="blue-text"><strong>Photos from the conference will be posted to Flickr.</strong></span> <span class="underline">Send us</span> your #hewebAL photos!</a>
+                </div>
+
 		    </div>
 
 		    <div class="col-xs-12 col-sm-6 col-md-12 col-lg-12">
 
 			    <div class="spotify-wrapper">
-				    <iframe src="https://embed.spotify.com/?uri=spotify:user:bamadesigner:playlist:02mU6Cls8XqGiuvnjIQkGT" width="300" height="350" frameborder="0" allowtransparency="true"></iframe>
+				    <iframe src="https://embed.spotify.com/?uri=spotify:user:bamadesigner:playlist:02mU6Cls8XqGiuvnjIQkGT" width="300" height="300" frameborder="0" allowtransparency="true"></iframe>
 				</div>
 
 			</div>
